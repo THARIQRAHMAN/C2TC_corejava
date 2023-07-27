9bcccc2ae5ec2f4f90487914e356143529beaaea
Thariq <Thariq@DESKTOP-I6C3JH8>
2023-07-27 10:49:02 +0530

    FIRST COMMIT

---
+++ C2TC/src/com/cg/dayone/BASICSJ.java
@@ -0,0 +1,28 @@
+package com.cg.dayone;
+import java.util.*;
+public class BASICSJ {
+	public static void main(String[] args) {
+		System.out.println("THE BOYS");
+		Scanner Sc = new Scanner(System.in);
+		System.out.print("Enter the 1st value:");
+		int a = Sc.nextInt();
+		System.out.print("Enter the 2nd value:");
+		int b = Sc.nextInt();
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		System.out.println(a<b);
+		System.out.println(a>b);
+		System.out.println(a&b);
+		System.out.println(a|b);
+		System.out.println(a<<b);
+		System.out.println(a>>b);
+		System.out.println(a++);
+		System.out.println(b++);
+		System.out.println(++a);
+		System.out.println(++b);
+	}
+
+}
