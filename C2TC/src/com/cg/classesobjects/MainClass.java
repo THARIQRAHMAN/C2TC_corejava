30097b3b39bcc89e20ba7bfd6860a2b93e25acc7
Thariq <Thariq@DESKTOP-I6C3JH8>
2023-07-28 10:50:49 +0530

    S

---
+++ C2TC/src/com/cg/classesobjects/MainClass.java
@@ -0,0 +1,16 @@
+package com.cg.classesobjects;
+import java.lang.*;
+public class MainClass {
+
+	public static void main(String[] args) {
+		MethodClass mv = new MethodClass();
+		int c = mv.add();
+		System.out.println(c);
+		int d = mv.sub();
+		System.out.println(d);
+		
+		
+		
+	}
+
+}
---
+++ C2TC/src/com/cg/classesobjects/MethodClass.java
@@ -0,0 +1,19 @@
+package com.cg.classesobjects;
+
+public class MethodClass {
+	int add() {
+		int a= 5;
+		int b = 3;
+		return a+b;
+	}
+	int sub() {
+		int a=3;
+		int b=7;
+		return a-b;
+	}
+	void mul() {
+		int a=4;
+		int b=7;
+		System.out.println(a*b);
+	}
+}
